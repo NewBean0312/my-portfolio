1c1c730c63d9b1c77f013ac0f7f8a9c2845f9921
NewBean0312 <newbean0312@gmail.com>
2023-09-13 16:22:58 +0900

    Main 페이지 UI 변경

--- jsFile/index.js
+++ jsFile/index.js
@@ -9,18 +9,31 @@ document.addEventListener("mousemove", (e) => {
 });
 
 // 타이핑 이벤트
-const content = "Hi. I'm YuBeen, Front-End Developer.";
-const text = document.querySelector(".text");
+const firstContent = "Hi. I'm YuBeen, Front-End Developer.";
+const firstText = document.querySelector(".text-1");
 let i = 0;
 
-function typing() {
-  if (i < content.length) {
-    let txt = content.charAt(i);
-    text.innerHTML += txt;
+function FirstTyping() {
+  if (i < firstContent.length) {
+    let txt = firstContent.charAt(i);
+    firstText.innerHTML += txt;
     i++;
   }
 }
-setInterval(typing, 150);
+setInterval(FirstTyping, 120);
+
+const secondContent = "My dream is to become a creative developer.";
+const secondText = document.querySelector(".text-2");
+let j = 0;
+
+function SecondTyping() {
+  if (j < secondContent.length) {
+    let txt = secondContent.charAt(j);
+    secondText.innerHTML += txt;
+    j++;
+  }
+}
+setInterval(SecondTyping, 100);
 
 // 화면 전환 이벤트
 jQuery(function ($) {
