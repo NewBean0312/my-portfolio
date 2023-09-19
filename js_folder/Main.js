00bc8896c4f8e7c594207764842ca3242fb55fb5
NewBean0312 <newbean0312@gmail.com>
2023-09-19 22:16:59 +0900

    Main, About 페이지 마우스 커서 제거

--- js_folder/Main.js
+++ js_folder/Main.js
@@ -1,13 +1,3 @@
-// 마우스 따라오는 이벤트
-const mouseCursor1 = document.querySelector(".mouse-cursor");
-
-document.addEventListener("mousemove", (e) => {
-  const mouseX = e.clientX;
-  const mouseY = e.clientY;
-  mouseCursor1.style.left = mouseX + 15 + "px";
-  mouseCursor1.style.top = mouseY + 15 + "px";
-});
-
 // 타이핑 이벤트
 const firstContent = "Hi. I'm YuBeen, Front-End Developer.";
 const firstText = document.querySelector(".text-1");
