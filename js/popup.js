36c6d1de9ddd36baf0d7f71bd0a47d1d03b4d5e3
NewBean0312 <newbean0312@gmail.com>
2023-11-01 11:42:34 +0900

    Project 페이지 팝업창 js 적용

---
+++ js/popup.js
@@ -0,0 +1,8 @@
+// 팝업창
+const pyeongChangPopupOpen = () => {
+  document.querySelector(".project-popup-section").classList.remove("hidden");
+};
+
+const pyeongChangPopupClose = () => {
+  document.querySelector(".project-popup-section").classList.add("hidden");
+};
